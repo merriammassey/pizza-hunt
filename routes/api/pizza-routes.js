a5f3ac18ef795fd5c4af3d1bcc5fff290e0b12ce
Maria M <prov21_1@hotmail.com>
2021-05-24 16:30:37 -0700

    api routes

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -1,11 +1,22 @@
 const router = require("express").Router();
+//Instead of importing the entire object and having to do pizzaController.getAllPizza(), we can simply destructure the method names out of the imported object and use those names directly.
+const {
+  getAllPizza,
+  getPizzaById,
+  createPizza,
+  updatePizza,
+  deletePizza,
+} = require("../../controllers/pizza-controller");
 
 // Set up GET all and POST at /api/pizzas
-router.route("/").get().post();
+// /api/pizzas
+//provide the name of the controller method as the callback
+
+router.route("/").get(getAllPizza).post(createPizza);
 
 // Set up GET one, PUT, and DELETE at /api/pizzas/:id
-router.route("/:id").get().put().delete();
-
+// /api/pizzas/:id
+router.route("/:id").get(getPizzaById).put(updatePizza).delete(deletePizza);
 module.exports = router;
 
 /*abstracts the database methods from the routes, giving us the option to write unit tests with Jest
