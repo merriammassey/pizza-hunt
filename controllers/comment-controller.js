1b59f46075b53c6b95a1a569a44e4ff0c43feee2
Maria M <prov21_1@hotmail.com>
2021-05-25 14:07:38 -0700

    comment modela nd route

---
+++ controllers/comment-controller.js
@@ -0,0 +1,54 @@
+const { Comment, Pizza } = require("../models");
+
+const commentController = {
+  // add comment to pizza
+  addComment({ params, body }, res) {
+    console.log(body);
+    Comment.create(body)
+      .then(({ _id }) => {
+        //add it to the Pizza
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          //add the comment's _id to the specific pizza
+          { $push: { comments: _id } },
+          //receive back updated pizza
+          { new: true }
+        );
+      })
+      //return Pizza promise
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No such pizza found" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
+  },
+  // remove comment
+  removeComment({ params }, res) {
+    Comment.findOneAndDelete({ _id: params.commentId })
+      .then((deletedComment) => {
+        if (!deletedComment) {
+          return res.status(404).json({ message: "No comment found" });
+        }
+        //remove comment from Pizza
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $pull: { comments: params.commentId } },
+          { new: true }
+        );
+      })
+      //return updated Pizza
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
+  },
+};
+
+module.exports = commentController;
--- models/Pizza.js
+++ models/Pizza.js
@@ -2,22 +2,44 @@
 const { Schema, model } = require("mongoose");
 
 //define fields with specific data types to regulate data appearance
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String,
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    createAt: {
+      type: Date,
+      default: Date.now,
+    },
+    size: {
+      type: String,
+      default: "Large",
+    },
+    toppings: [],
+    //expect an ObjectId and to tell it that its data comes from the Comment model
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        //tell the Pizza model which documents to search to find the right comment
+        ref: "Comment",
+      },
+    ],
   },
-  createdBy: {
-    type: String,
-  },
-  createAt: {
-    type: Date,
-    default: Date.now,
-  },
-  size: {
-    type: String,
-    default: "Large",
-  },
-  toppings: [],
+  //tell schema to use virtuals for comment count
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// with a virtual, get total count of comments and replies on retrieval
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
 });
 
 // create the Pizza model using the PizzaSchema
