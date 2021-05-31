a00f965bdf1ed97a0b112469d2f534188799eafd
Maria M <prov21_1@hotmail.com>
2021-05-31 11:26:23 -0700

    pizza data validation

--- models/Pizza.js
+++ models/Pizza.js
@@ -9,9 +9,13 @@ const PizzaSchema = new Schema(
   {
     pizzaName: {
       type: String,
+      required: true,
+      trim: true,
     },
     createdBy: {
       type: String,
+      required: true,
+      trim: true,
     },
     createdAt: {
       type: Date,
@@ -21,6 +25,8 @@ const PizzaSchema = new Schema(
     },
     size: {
       type: String,
+      required: true,
+      enum: ["Personal", "Small", "Medium", "Large", "Extra Large"],
       default: "Large",
     },
     toppings: [],
