27f9a6fc9541ba75cd9145c66b8652aa1310ef10
Maria M <prov21_1@hotmail.com>
2021-05-31 18:35:20 -0700

    updated connection

--- server.js
+++ server.js
@@ -8,13 +8,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(require("./routes"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pizza-hunt", {
+/* mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pizza-hunt", {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}); */
 
 // Use this to log mongo queries being executed!
 mongoose.set("debug", true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+
+const connection =
+  "mongodb+srv://mmm97m:mebechjo!1@cluster0/pizza-hunt-db?retryWrites=true&w=majority";
+mongoose
+  .connect(connection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Database Connected Successfully"))
+  .catch((err) => console.log(err));
